f6940cd4ae2742f35dacae2770a190b1d212f90d
Vineet <vineetbattula@gmail.com>
2019-05-10 13:59:19 -0400

    Created model

---
+++ assesment3/src/main/java/com/vineet/assessment3/controller/UserEventController.java
@@ -0,0 +1,12 @@
+package com.vineet.assessment3.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserEventController {
+
+
+}
+
+
+
---
+++ assesment3/src/main/java/com/vineet/assessment3/model/UserEvent.java
@@ -0,0 +1,39 @@
+package com.vineet.assessment3.model;
+
+public class UserEvent {
+
+    private String type;
+    private String userId;
+    private long timestamp;
+    private String context;
+
+    public String getType() {
+        return type;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+    public String getUserId() {
+        return userId;
+    }
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+    public long getTimestamp() {
+        return timestamp;
+    }
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+    public String getContext() {
+        return context;
+    }
+    public void setContext(String context) {
+        this.context = context;
+    }
+
+
+
+
+}
+}
